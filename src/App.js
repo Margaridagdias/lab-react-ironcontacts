97b6635d7b788b244d1002fbbfd900d1470d8b43
Margarida <margarida97pt@gmail.com>
2020-11-25 17:59:49 +0000

    almost done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: contacts.splice(0, 5),
+  };
+
+  addNewContactToTable = () => {
+    let newArray = [...this.state.contacts];
+
+    let index = Math.floor(Math.random() * contacts.length);
+    let newContact = contacts[index];
+
+    newArray.push(newContact);
+
+    this.setState({
+      contacts: newArray,
+    });
+  };
+
+  deleteContact = (id) => {
+    //copy state to another variable
+    const newArray = [...this.state.contacts];
+
+    //get the index of the movie we want to delete
+    const contactToRemoveIndex = newArray.findIndex((contact) => {
+      return contact.id === id;
+    });
+
+    newArray.splice(contactToRemoveIndex, 1);
+    this.setState({
+      contacts: newArray,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={this.addNewContactToTable}>Add new contact</button>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+        </table>
+
+        <tbody>
+          {this.state.contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt={contact.name}
+                    width="100px"
+                  />
+                </td>
+                <td>
+                  <h3>{contact.name}</h3>
+                </td>
+                <td>
+                  <h3>{Math.round(contact.popularity)}</h3>
+                </td>
+                <button onClick={() => this.deleteContact(contact.id)}>
+                  delete this contact
+                </button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </div>
+    );
+  }
 }
-
 export default App;
